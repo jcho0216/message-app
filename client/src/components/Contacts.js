1b1e988223075950d0bbf229bf38c91de704af31
Jun Joy <chojuneseo1@gmail.com>
2021-06-24 14:21:22 +0900

    update; Contacts component

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React from 'react'
+import { ListGroup } from 'react-bootstrap'
+import { useContacts } from '../contexts/ContactsProvider';
 
-function Contacts() {
-    return (
-        <div>Contacts</div>
-    )
+export default function Contacts() {
+  const { contacts } = useContacts()
+
+  return (
+    <ListGroup variant="flush">
+      {contacts.map(contact => (
+        <ListGroup.Item key={contact.id}>
+          {contact.name}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  )
 }
-
-export default Contacts;
