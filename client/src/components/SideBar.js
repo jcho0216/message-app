f6c8ad508deedd2645da4c5731a58d5304b93795
Jun Joy <chojuneseo1@gmail.com>
2021-06-27 23:36:04 +0900

    update; SideBar.js

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
-import { Tab, Nav, Button, Modal } from "react-bootstrap";
-import Contacts from "./Contacts";
-import Conversations from './Conversations';
+import React, { useState } from 'react'
+import { Tab, Nav, Button, Modal } from 'react-bootstrap'
+import Conversations from './Conversations'
+import Contacts from './Contacts'
+import NewContactModal from './NewContactModal'
+import NewConversationModal from './NewConversationModal'
 
-const CONVERSATIONS_KEY = "conversations";
-const CONTACTS_KEY = "contacts";
+const CONVERSATIONS_KEY = 'conversations'
+const CONTACTS_KEY = 'contacts'
 
 export default function Sidebar({ id }) {
-    const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
-    const conversationsOpen = activeKey === CONVERSATIONS_KEY
+  const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+  const [modalOpen, setModalOpen] = useState(false)
+  const conversationsOpen = activeKey === CONVERSATIONS_KEY
+  
+  function closeModal() {
+    setModalOpen(false)
+  }
 
   return (
-    <div style={{ width: "250px" }} className="d-flex flex-column">
+    <div style={{ width: '250px' }} className="d-flex flex-column">
       <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
         <Nav variant="tabs" className="justify-content-center">
           <Nav.Item>
@@ -22,23 +29,27 @@ export default function Sidebar({ id }) {
           </Nav.Item>
         </Nav>
         <Tab.Content className="border-right overflow-auto flex-grow-1">
-           <Tab.Pane eventKey={CONVERSATIONS_KEY}>
-               <Conversations />
-           </Tab.Pane>
-           <Tab.Pane eventKey={CONTACTS_KEY}>
-               <Contacts />
-           </Tab.Pane>
+          <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+            <Conversations />
+          </Tab.Pane>
+          <Tab.Pane eventKey={CONTACTS_KEY}>
+            <Contacts />
+          </Tab.Pane>
         </Tab.Content>
-        <div className="p-2 border-top border-right small">Your id: <span className="text-muted">{id}</span></div>
-        <Button className="rounded-0">
-            New {conversationsOpen ? 'Conversation' : 'Conact'}
+        <div className="p-2 border-top border-right small">
+          Your Id: <span className="text-muted">{id}</span>
+        </div>
+        <Button onClick={() => setModalOpen(true)} className="rounded-0">
+          New {conversationsOpen ? 'Conversation' : 'Contact'}
         </Button>
       </Tab.Container>
 
-      <Modal>
-          {conversationsOpen ?
-          <}
+      <Modal show={modalOpen} onHide={closeModal}>
+        {conversationsOpen ?
+          <NewConversationModal closeModal={closeModal} /> :
+          <NewContactModal closeModal={closeModal} />
+        }
       </Modal>
     </div>
-  );
+  )
 }
